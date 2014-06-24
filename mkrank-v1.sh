5366fc951bdd95aea38f86186b1bb1d7973e0417
ryan35205 <ryanrod.hg@gmail.com>
2014-06-24 18:12:38 -0500

    Delete mkrank-v1.sh

--- mkrank-v1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rank=0
-/cm/shared/apps/pbspro/11.2.0.113417/bin/qstat -f $PBS_JOBID |tr -d '\n'| tr -d '\t'|grep -oP 'compute[0-4]{2}.cm.cluster/[0-7]' | sed 's/.cm.cluster\// /g' |while read node core
-do echo "rank $((rank ++))=$node slot=$core" >> /home/xiss/Testing/LS_Dyna/LS_Bench/rankfile
-done
